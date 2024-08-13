adfe602a11bc42bdad50758929c7b0de797ba7e2
imranhasan2 <imranhasan030303@gmail.com>
2024-08-13 16:30:13 +0600

    added delete fnc

--- index.js
+++ index.js
@@ -107,6 +107,26 @@ async function run() {
     res.send (result)
   })
 
+  // get all room for host
+
+  app.get('/my-listings/:email',async (req,res) =>{
+    const email=req.params.email
+    const query ={'host.email':email}
+    const result =await roomCollection.find(query).toArray()
+    res.send (result)
+  })
+
+
+  // delete room
+
+  app.delete('/room/:id',async(req,res) =>{
+    const id =req.params.id
+    console.log(id)
+    const query ={_id : new ObjectId(id)}
+    const result =await roomCollection.deleteOne(query)
+    res.send(result)
+  })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
