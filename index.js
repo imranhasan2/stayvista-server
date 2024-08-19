9ff95b2175cef19feae3053e8d76b5af7bf9e6ad
imranhasan2 <imranhasan030303@gmail.com>
2024-08-19 19:22:41 +0600

    get all users from db

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const verifyToken = async (req, res, next) => {
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Main`
 
-const uri =process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -50,7 +50,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    const roomCollection =client.db('stayvista').collection('rooms')
+    const roomCollection = client.db('stayvista').collection('rooms')
+    const userCollection = client.db('stayvista').collection('user')
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body
@@ -82,50 +83,96 @@ async function run() {
     })
 
 
+    // save a user data in db
+
+    app.put('/user', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+    
+      // Check if the user already exists
+      const isExist = await userCollection.findOne(query);
+    
+      if (isExist) {
+        // If the user exists and the status is 'Requested', update only the status
+        if (user.status === 'Requested') {
+          const result = await userCollection.updateOne(query, {
+            $set: { status: user?.status },
+          });
+          return res.send(result);
+        }
+        // Return the existing user data to avoid unnecessary operations
+        return res.send(isExist);
+      }
+    
+      // If the user doesn't exist, create a new entry
+      const options = { upsert: true };
+    
+      const updateDoc = {
+        $set: {
+          ...user,              // Set the user data
+          timestamp: new Date(), // Add a timestamp to the user's document
+        },
+      };
+    
+      try {
+        const result = await userCollection.updateOne(query, updateDoc, options);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Error saving user', error });
+      }
+    });
+    
+    // get all user
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     // get all Rooms data
 
-  app.get('/rooms',async(req,res) =>{
-    const category =req.query.category;
-    let query ={}
-    if(category && category !== 'null') query ={ category }
-    const result =await roomCollection.find(query).toArray()
-    res.send(result)
-  })
+    app.get('/rooms', async (req, res) => {
+      const category = req.query.category;
+      let query = {}
+      if (category && category !== 'null') query = { category }
+      const result = await roomCollection.find(query).toArray()
+      res.send(result)
+    })
 
-  // get single room data using id
+    // get single room data using id
 
-  app.get('/room/:id',async(req,res) =>{
-    const id =req.params.id
-    const query ={_id :new ObjectId(id)}
-    const result = await roomCollection.findOne(query)
-    res.send (result)
-  })
+    app.get('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await roomCollection.findOne(query)
+      res.send(result)
+    })
 
-  app.post('/room',async(req,res) =>{
-    const roomData = req.body;
-    const result =await roomCollection.insertOne(roomData)
-    res.send (result)
-  })
+    app.post('/room', async (req, res) => {
+      const roomData = req.body;
+      const result = await roomCollection.insertOne(roomData)
+      res.send(result)
+    })
 
-  // get all room for host
+    // get all room for host
 
-  app.get('/my-listings/:email',async (req,res) =>{
-    const email=req.params.email
-    const query ={'host.email':email}
-    const result =await roomCollection.find(query).toArray()
-    res.send (result)
-  })
+    app.get('/my-listings/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { 'host.email': email }
+      const result = await roomCollection.find(query).toArray()
+      res.send(result)
+    })
 
 
-  // delete room
+    // delete room
 
-  app.delete('/room/:id',async(req,res) =>{
-    const id =req.params.id
-    console.log(id)
-    const query ={_id : new ObjectId(id)}
-    const result =await roomCollection.deleteOne(query)
-    res.send(result)
-  })
+    app.delete('/room/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await roomCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
